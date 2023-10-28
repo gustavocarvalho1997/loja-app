a138267b1a5918b3516d3696e5648727c68d1b6f
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-10-27 21:17:09 -0300

    post api e handle change/submit

--- src/app/api/base/base-user-api/route.jsx
+++ src/app/api/base/base-user-api/route.jsx
@@ -1,9 +1,23 @@
 import {promises as fs} from 'fs';
 import { NextResponse } from 'next/server';
-
+//Recuperar o arquivo json.
+const file = await fs.readFile(process.cwd() + '/src/app/api/base/db.json', 'utf-8');
 export async function GET(request,{params}) {
-
-    //Recuperar o arquivo json.
-    const file = await fs.readFile(process.cwd() + '/src/app/api/base/db.json', 'utf-8');
+  
     return NextResponse.json(JSON.parse(file));
+}
+
+export async function POST(request, response) {
+    // PARSEANDO O ARQUIVO PARA O JSON NATIVO!!!
+    const body = await JSON.parse(file);
+    const usuario = await request.json();
+    for (let x = 0; x < body.usuarios.length; x++) {
+        const u = body.usuarios[x];
+        //console.log("USUARIO DA BASE : ", u.name);
+        if(u.email == usuario.email && u.senha == usuario.senha){
+            return NextResponse.json({"status": "ok"});
+        }
+    }
+    return NextResponse.json({"status": "error"})
+    
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,32 +1,68 @@
+"use client"
+import { useState } from "react";
 
-export default async function LoginUsers() {
+export default function LoginUsers() {
+    //Mensagem de status
+    const [msg, setMsg] = useState("");
 
-    let users;
-
-    //Recuperar o usuário que está em nossa base json.
-    try{
-        const response = await fetch("http://localhost:3000/api/base/base-user-api");
-        users = await response.json();
-        console.log(users);
-    }catch(error){
-        console.log(error);
+    const [usuario, setUsuario] = useState({
+        email: "",
+        senha: ""
+    })
+    //preenchimento dos campos
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUsuario({...usuario,[name]:value})
     }
+    //envio das informações
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await fetch("http://localhost:3000/api/base/base-user-api", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(usuario)
+            });
+            if(response.ok){
+                const result = await response.json();
+                console.log(result.status)
+                if(result.status == "ok"){
+                    setMsg("Login efetuado com sucesso!")
+                    setTimeout(() => {
+                        setMsg("");
+                    }, 5000);
+                } else {
+                    setMsg("Login ou senha incorretos")
+                    setTimeout(() => {
+                        setMsg("");
+                    }, 5000); 
+                }
+            }
+            console.log(users);
+        }catch(error){
+            console.log(error);
+        }
+    }
+    //Recuperar o usuário que está em nossa base json.
+    
 
   return (
     <div>
         <h1>IDENTIFICAÇÃO DOS USUÁRIOS</h1>
-        
+        <h2 className="bg-red-300 text-red-700 text-center text-lg">{msg}</h2>
         <div className="form-login">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>LOGIN</legend>
                     <div>
-                        <label htmlFor="idUser">Usuário:</label>
-                        <input type="text" name="usuario" id="idUser" placeholder="Digite seu nome de usuário."/>
+                        <label htmlFor="idEmail">Usuário:</label>
+                        <input type="email" name="email" id="idEmail" placeholder="Digite seu email." value={usuario.email} onChange={handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="idPassword">Senha:</label>
-                        <input type="password" name="senha" id="idPassword" placeholder="Digite sua senha."/>
+                        <label htmlFor="idSenha">Senha:</label>
+                        <input type="password" name="senha" id="idSenha" placeholder="Digite sua senha." value={usuario.senha} onChange={handleChange}/>
                     </div>
                     <div>
                         <button>LOGIN</button>
